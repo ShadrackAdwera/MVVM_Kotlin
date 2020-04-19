85b097cefa3b89241adc5d9abc3453560b6b7ceb
ShadrackAdwera <adweshshaddie@gmail.com>
2020-04-19 19:32:16 +0300

    data binding in the views

--- app/src/main/java/com/adwera/mvvmkotlin/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/adwera/mvvmkotlin/ui/auth/AuthViewModel.kt
@@ -1,4 +1,17 @@
 package com.adwera.mvvmkotlin.ui.auth
 
-class AuthViewModel {
+import android.view.View
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+    var email : String? = null
+    var password: String? = null
+
+    fun onLoginClicked (view: View) {
+        if(email.isNullOrEmpty() || password.isNullOrEmpty()){
+            //error message
+            return
+        }
+        //successful login
+    }
 }
