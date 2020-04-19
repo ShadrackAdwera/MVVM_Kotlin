fcc0b19581624350399197dd2e373e60363bc4ad
ShadrackAdwera <adweshshaddie@gmail.com>
2020-04-19 17:32:07 +0300

    Create layout files

---
+++ app/src/main/java/com/adwera/mvvmkotlin/ui/auth/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.adwera.mvvmkotlin.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adwera.mvvmkotlin.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
--- app/src/main/java/com/adwera/mvvmkotlin/ui/home/MainActivity.kt
+++ app/src/main/java/com/adwera/mvvmkotlin/ui/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.adwera.mvvmkotlin
+package com.adwera.mvvmkotlin.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.adwera.mvvmkotlin.R
 
 class MainActivity : AppCompatActivity() {
 
