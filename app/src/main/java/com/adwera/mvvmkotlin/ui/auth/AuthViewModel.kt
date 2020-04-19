7d428dccaf17c20ff9def3aa911443b4f9f1ddde
ShadrackAdwera <adweshshaddie@gmail.com>
2020-04-19 20:09:40 +0300

    bind data to the views

--- app/src/main/java/com/adwera/mvvmkotlin/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/adwera/mvvmkotlin/ui/auth/AuthViewModel.kt
@@ -7,11 +7,14 @@ class AuthViewModel : ViewModel() {
     var email : String? = null
     var password: String? = null
 
+    var authListener : AuthListener? = null
+
     fun onLoginClicked (view: View) {
+        authListener?.onStarted()
         if(email.isNullOrEmpty() || password.isNullOrEmpty()){
-            //error message
+            authListener?.onFailure("Invalid Email/Password")
             return
         }
-        //successful login
+        authListener?.onSuccess()
     }
 }
--- app/src/main/java/com/adwera/mvvmkotlin/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/adwera/mvvmkotlin/ui/auth/LoginActivity.kt
@@ -2,12 +2,31 @@ package com.adwera.mvvmkotlin.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.adwera.mvvmkotlin.R
+import com.adwera.mvvmkotlin.databinding.ActivityLoginBinding
+import com.adwera.mvvmkotlin.utils.toast
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), AuthListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding : ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        val viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
+        binding.viewmodel = viewModel
+        viewModel.authListener = this
+    }
+
+    override fun onStarted() {
+        toast("Login Started")
+    }
+
+    override fun onSuccess() {
+        toast("Successful login")
+    }
+
+    override fun onFailure(message:String) {
+       toast(message)
     }
 }
