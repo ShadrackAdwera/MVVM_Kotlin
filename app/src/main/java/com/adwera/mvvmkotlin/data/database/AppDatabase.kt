0ab4c90060f069c19730842e498bfb415cc4dfc6
ShadrackAdwera <adweshshaddie@gmail.com>
2020-04-20 10:53:08 +0300

    set up the database

--- app/src/main/java/com/adwera/mvvmkotlin/data/database/AppDatabase.kt
+++ app/src/main/java/com/adwera/mvvmkotlin/data/database/AppDatabase.kt
@@ -1,5 +1,26 @@
 package com.adwera.mvvmkotlin.data.database
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.adwera.mvvmkotlin.data.database.entities.User
 
+@Database(entities = [ User::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun getUserDao() : UserDao
+
+    companion object {
+        @Volatile
+        private var instance : AppDatabase? = null
+        private val LOCK  = Any()
+
+        operator fun invoke(context: Context) = instance?: synchronized(LOCK){
+            instance?:buildDatabase(context).also {
+                instance = it
+            }
+        }
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java,"UserDatabase.db").build()
+    }
 }
